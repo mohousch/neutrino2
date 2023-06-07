f1acb30814888b8cf5e38e82229b40c2d2eaea6f
mohousch <mohousch@gmail.com>
2023-06-07 18:23:25 +0200

    - supplement to imageinfo widget.

--- neutrino2/src/gui/imageinfo.cpp
+++ neutrino2/src/gui/imageinfo.cpp
@@ -246,7 +246,7 @@ void CImageInfo::paint()
 	std::string Text = "This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\nSee the GNU General Public License for more details.\nYou should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.";
 	
 	CCText licence;
-	licence.setPosition(xpos + x_offset, ypos, width - x_offset, height - 2*iheight);
+	licence.setPosition(xpos + x_offset, ypos, width - x_offset - 20, height - 2*iheight);
 	licence.setText(Text.c_str());
 	
 	licence.paint();
