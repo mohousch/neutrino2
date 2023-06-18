954c38978046767c669413aa356d555f985cfa55
mohousch <mohousch@gmail.com>
2023-06-18 11:32:18 +0200

    - cleanup.

--- neutrino2/src/gui/widget/listbox.cpp
+++ neutrino2/src/gui/widget/listbox.cpp
@@ -927,7 +927,7 @@ CMenuSeparator::CMenuSeparator(const int Type, const char * const Text)
 	itemName = Text? Text : "";
 	
 	//
-	color = COL_MENUCONTENTDARK_PLUS_0;
+	color = COL_MENUCONTENT_PLUS_5; //COL_MENUCONTENTDARK_PLUS_0;
 	gradient = NOGRADIENT;
 
 	menuItem_type = MENUITEM_SEPARATOR;
@@ -1001,26 +1001,26 @@ int CMenuSeparator::paint(bool /*selected*/, bool /*AfterPulldown*/)
 					if (type & ALIGN_LEFT)
 					{
 						//
-						frameBuffer->paintBoxRel(x + BORDER_LEFT + stringwidth + BORDER_LEFT, y + (height - 2)/2, dx - BORDER_LEFT - BORDER_RIGHT - stringwidth, 2, color, 0, CORNER_NONE, gradient, GRADIENT_VERTICAL);
+						frameBuffer->paintBoxRel(x + BORDER_LEFT + stringwidth + BORDER_LEFT, y + (height - 2)/2, dx - BORDER_LEFT - BORDER_RIGHT - stringwidth, 2, color, 0, CORNER_NONE, gradient, GRADIENT_HORIZONTAL);
 					}
 					else if (type & ALIGN_RIGHT)
 					{
 						//
-						frameBuffer->paintBoxRel(x + BORDER_LEFT, y + (height - 2)/2, dx - BORDER_LEFT - BORDER_RIGHT - stringwidth, 2, color, 0, CORNER_NONE, gradient, GRADIENT_VERTICAL);
+						frameBuffer->paintBoxRel(x + BORDER_LEFT, y + (height - 2)/2, dx - BORDER_LEFT - BORDER_RIGHT - stringwidth, 2, color, 0, CORNER_NONE, gradient, GRADIENT_HORIZONTAL);
 					}
 					else // ALIGN_CENTER
 					{
 						// left
-						frameBuffer->paintBoxRel(x + BORDER_LEFT, y + (height - 2)/2, (dx - BORDER_LEFT - BORDER_RIGHT - stringwidth)/2 - BORDER_LEFT, 2, color, 0, CORNER_NONE, gradient, GRADIENT_VERTICAL);
+						frameBuffer->paintBoxRel(x + BORDER_LEFT, y + (height - 2)/2, (dx - BORDER_LEFT - BORDER_RIGHT - stringwidth)/2 - BORDER_LEFT, 2, color, 0, CORNER_NONE, gradient, GRADIENT_HORIZONTAL);
 						
 						// right
-						frameBuffer->paintBoxRel(x + (dx + stringwidth)/2 + BORDER_LEFT, y + (height - 2)/2, (dx - BORDER_LEFT - BORDER_RIGHT - stringwidth)/2 - BORDER_RIGHT, 2, color, 0, CORNER_NONE, gradient, GRADIENT_VERTICAL);
+						frameBuffer->paintBoxRel(x + (dx + stringwidth)/2 + BORDER_LEFT, y + (height - 2)/2, (dx - BORDER_LEFT - BORDER_RIGHT - stringwidth)/2 - BORDER_RIGHT, 2, color, 0, CORNER_NONE, gradient, GRADIENT_HORIZONTAL);
 					}
 				}
 			}
 			else
 			{
-				frameBuffer->paintBoxRel(x + BORDER_LEFT, y + (height - 2)/2, dx - BORDER_LEFT - BORDER_RIGHT, 2, color, 0, CORNER_NONE, gradient, GRADIENT_VERTICAL);
+				frameBuffer->paintBoxRel(x + BORDER_LEFT, y + (height - 2)/2, dx - BORDER_LEFT - BORDER_RIGHT, 2, color, 0, CORNER_NONE, gradient, GRADIENT_HORIZONTAL);
 			}	
 		}
 	}
--- neutrino2/src/system/debug.h
+++ neutrino2/src/system/debug.h
@@ -25,16 +25,7 @@
 #ifndef __neutrino_debug__
 #define __neutrino_debug__
 
-/*
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_WHITE   "\x1b[37m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
-*/
+
 #define ANSI_RESET   "\33[0m"
 #define ANSI_BOLD    "\33[1m"
 #define ANSI_BLINK   "\33[5m"
