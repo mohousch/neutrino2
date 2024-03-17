2ce9a723b7b28f1f5f50768bf7b1000ea32a38d2
mohousch <mohousch@gmail.com>
2024-03-17 12:29:39 +0100

    Show DVB Subtitle background.

--- neutrino2/lib/libsubtitle/dvbsubtitle.cpp
+++ neutrino2/lib/libsubtitle/dvbsubtitle.cpp
@@ -103,7 +103,7 @@ void cDvbSubtitleBitmaps::Draw(int &min_x, int &min_y, int &max_x, int &max_y)
 #endif
 		
 		// blit2fb
-		CFrameBuffer::getInstance()->blit2FB(newdata, nw, nh, xoff, yoff, 0, 0, true);
+		CFrameBuffer::getInstance()->blit2FB(newdata, nw, nh, xoff, yoff, 0, 0, false);
 		CFrameBuffer::getInstance()->blit();
 
 		free(newdata);
