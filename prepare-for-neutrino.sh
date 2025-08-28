c1c1b6a8179c0fa42d3e442be36fa94135528a38
mohousch <mohousch@gmail.com>
2025-08-28 19:24:57 +0200

    -Update.

--- prepare-for-neutrino.sh
+++ prepare-for-neutrino.sh
@@ -144,28 +144,10 @@ PACKAGES="\
     	${UBUNTU:+libass-dev}											      \
     	${UBUNTU:+libglew-dev}											      \
     	${UBUNTU:+freeglut3-dev}										      \
-    	${UBUNTU:+libao-dev}
-	";
-
-if [ "$UBUNTU" == 1 ]; then
-	UBUNTU_VERSION=`lsb_release -r | grep "Release" | cut -f2 | cut -d . -f1`
-elif [ "$UBUNTU" == 2 ]; then
-	MINT_VERSION=`lsb_release -r | grep "Release" | cut -f2 | cut -d . -f1`
-fi
-# debian / linuxmint lmde
-if ([ "$UBUNTU" == 1  ] &&  [ "$UBUNTU_VERSION" -ge "12" ]) || ([ "$UBUNTU" == 2 ] && [ "$MINT_VERSION" -ge "6" ]); then
-	PACKAGES="$PACKAGES \
-	${UBUNTU:+libtool-bin} \
+    	${UBUNTU:+libao-dev}											      \
+    	${UBUNTU:+libtool-bin} 											      \
 	${UBUNTU:+gtk-doc-tools}
 	";
-fi
-# ubuntu / linuxmint
-if ([ "$UBUNTU" == 1  ] &&  [ "$UBUNTU_VERSION" -ge "23" ]) || ([ "$UBUNTU" == 2 ] && [ "$MINT_VERSION" -ge "21" ]); then
-	PACKAGES="$PACKAGES \
-	${UBUNTU:+libtool-bin} \
-	${UBUNTU:+gtk-doc-tools}
-	";
-fi
 
 if [ `which arch > /dev/null 2>&1 && arch || uname -m` == x86_64 ]; then
 	# ST changed to the -m32 option for their gcc compiler build
@@ -179,6 +161,7 @@ if [ `which arch > /dev/null 2>&1 && arch || uname -m` == x86_64 ]; then
 	                                ${SUSE:+libstdc++-devel-32bit}  ${FEDORA:+glibc-static.i686}
 	";
 fi
+
 echo $INSTALL $PACKAGES
 $INSTALL $PACKAGES
 
