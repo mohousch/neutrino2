b24bf58f3827f499b6d20f313da46de28c6c703a
mohousch <mohousch@gmail.com>
2025-11-08 09:54:46 +0100

    - Fix last commit.

--- neutrino2/src/daemonc/remotecontrol.cpp
+++ neutrino2/src/daemonc/remotecontrol.cpp
@@ -127,6 +127,8 @@ int CRemoteControl::handleMsg(const neutrino_msg_t msg, neutrino_msg_data_t data
 		     (msg == NeutrinoMessages:: EVT_ZAP_SUB_FAILED) ) 
 	{
 		dprintf(DEBUG_NORMAL, "CRemoteControl::handleMsg: %s current_channel_id: 0x%llx data:0x%llx\n", (msg == NeutrinoMessages::EVT_ZAP_FAILED)? "EVT_ZAP_FAILED" : "EVT_ZAP_COMPLETE[1]", CZapit::getInstance()->getCurrentChannelID(), data);
+		current_channel_id = data;
+		
 		////
 		if ( (msg == NeutrinoMessages::EVT_ZAP_SUB_COMPLETE) || (msg == NeutrinoMessages:: EVT_ZAP_SUB_FAILED) ) 
 		{
