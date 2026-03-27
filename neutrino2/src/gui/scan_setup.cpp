6869926d0a2f3642ebb7371dc7325df0bc869325
mohousch <mohousch@gmail.com>
2026-03-27 13:04:47 +0100

    - [scanSetup] small fix.
    - handle unknown delivery system case.

--- neutrino2/src/gui/scan_setup.cpp
+++ neutrino2/src/gui/scan_setup.cpp
@@ -1,34 +1,33 @@
-/*
-	$Id: scan_setup.cpp 10082024 mohousch Exp $
-
-	Neutrino-GUI  -   DBoxII-Project
-
-	scan setup implementation - Neutrino-GUI
-
-	Copyright (C) 2001 Steffen Hehn 'McClean'
-	and some other guys
-	Homepage: http://dbox.cyberphoria.org/
-
-	Copyright (C) 2009 T. Graf 'dbt'
-	Homepage: http://www.dbox2-tuning.net/
-
-	License: GPL
-
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-*/
+//
+//	$Id: scan_setup.cpp 10082024 mohousch Exp $
+//
+//	Neutrino-GUI  -   DBoxII-Project
+//
+//	scan setup implementation - Neutrino-GUI
+//
+//	Copyright (C) 2001 Steffen Hehn 'McClean'
+//	and some other guys
+//	Homepage: http://dbox.cyberphoria.org/
+//
+//	Copyright (C) 2009 T. Graf 'dbt'
+//	Homepage: http://www.dbox2-tuning.net/
+//
+//	License: GPL
+//
+//	This program is free software; you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation; either version 2 of the License, or
+//	(at your option) any later version.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program; if not, write to the Free Software
+//	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -2309,7 +2308,16 @@ bool CScanSetupDelSysNotifier::changeNotify(const std::string&, void *Data)
 {
 	uint32_t delsys = *((uint32_t*) Data);
 	
-	if (delsys == CFrontend::DVB_T || delsys == CFrontend::DVB_T2 || delsys == CFrontend::DVB_DTMB)
+	if (delsys == CFrontend::UNDEFINED)
+	{
+		item1->setHidden(true);
+		item2->setHidden(true);
+		item3->setHidden(true);
+		item4->setHidden(true);
+		item5->setHidden(true);
+		item6->setHidden(true);
+	}
+	else if (delsys == CFrontend::DVB_T || delsys == CFrontend::DVB_T2 || delsys == CFrontend::DVB_DTMB)
 	{
 		item1->setHidden(false);
 		item2->setHidden(true);
