e5130e31eab46e39d1502853168bc57a49b7ba11
mohousch <mohousch@gmail.com>
2026-09-15 18:23:52 +0200

    - Cleanup.

--- neutrino2/src/driver/rcinput.cpp
+++ neutrino2/src/driver/rcinput.cpp
@@ -62,7 +62,13 @@ const char * const RC_EVENT_DEVICE[NUMBER_OF_EVENT_DEVICES] = {
 	"/dev/input/event0", 
 	"/dev/input/event1", 
 	"/dev/input/event2", 
-	"/dev/input/event3"
+	"/dev/input/event3",
+	"/dev/input/event4",
+	"/dev/input/event5",
+	"/dev/input/event6",
+	"/dev/input/event7",
+	"/dev/input/event8",
+	"/dev/input/event9"
 };
 
 typedef struct input_event t_input_event;
